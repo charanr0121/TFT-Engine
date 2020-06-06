93a0edfde128a6a43579f7ed3b767332f3caa3ce
Charan Ramachandran <charan@MacBook-Pro.local>
2020-06-06 17:46:18 -0400

    backend dir

---
+++ backend/logic.js
@@ -0,0 +1,16 @@
+
+var champsList = require('../api/set3/champions.js')
+
+//param: list of champions owned (on bench)
+function findSynergy(currentBench){
+    console.log(currentBench)
+}
+
+console.log(champsList)
+//findSynergy("hi")
+
+/* when suggesting what champion to pursue, take a look
+at the current synergies. if there already are badges, try
+to add more to those badges. if there are no badges, try to get
+a badge. 
+*/
